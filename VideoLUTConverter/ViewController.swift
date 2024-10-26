de6d91cde91c8127812cd9d99db1d7aa7dc1dcf8
raama srivatsan <raama@me.com>
2024-10-25 18:03:53 -0700

    Initial Commit

---
+++ VideoLUTConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoLUTConverter
+//
+//  Created by raama srivatsan on 10/25/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ VideoLUTConverterTests/VideoLUTConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLUTConverterTests.swift
+//  VideoLUTConverterTests
+//
+//  Created by raama srivatsan on 10/25/24.
+//
+
+import Testing
+@testable import VideoLUTConverter
+
+struct VideoLUTConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
